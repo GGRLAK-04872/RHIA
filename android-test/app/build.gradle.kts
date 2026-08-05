309526d75275c2d964b3daf133b5b9cf9e420933
GGRLAK-04872 <brunsmike92@gmail.com>
2026-08-05 15:17:53 +0200

    Android-Sprachsteuerung vollständig nativ ausführen

--- android-test/app/build.gradle.kts
+++ android-test/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "app.rhia.localtest"
         minSdk = 31
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
     }
 }
