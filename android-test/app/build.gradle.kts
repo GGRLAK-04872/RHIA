3c0ca627a3f576d003953264319b1bec8df0e9fa
GGRLAK-04872 <brunsmike92@gmail.com>
2026-08-05 20:56:29 +0200

    Bump RHIA Android test to 0.16

--- android-test/app/build.gradle.kts
+++ android-test/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "app.rhia.localtest"
         minSdk = 31
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.15"
+        versionCode = 16
+        versionName = "0.16"
     }
 }
