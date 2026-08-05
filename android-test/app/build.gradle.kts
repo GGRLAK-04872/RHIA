d2d08a43d9a26e7e8087351bf36a6b9dcf355a41
GGRLAK-04872 <brunsmike92@gmail.com>
2026-08-05 14:08:53 +0200

    Android-Testversion auf 0.3 erhöhen

--- android-test/app/build.gradle.kts
+++ android-test/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "app.rhia.localtest"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
 }
