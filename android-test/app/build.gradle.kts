86786354775d38f200d267b7a03e03d64e472891
GGRLAK-04872 <brunsmike92@gmail.com>
2026-08-05 15:33:27 +0200

    RHIA Android Local Test 0.5 vorbereiten

--- android-test/app/build.gradle.kts
+++ android-test/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "app.rhia.localtest"
         minSdk = 31
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4"
+        versionCode = 5
+        versionName = "0.5"
     }
 }
