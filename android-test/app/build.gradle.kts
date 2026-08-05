599add1aee24181a73ac4a5e5cfecf6f8e8aad28
GGRLAK-04872 <brunsmike92@gmail.com>
2026-08-05 17:52:32 +0200

    Android-Testversion auf 0.10 erhöhen

--- android-test/app/build.gradle.kts
+++ android-test/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "app.rhia.localtest"
         minSdk = 31
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.9"
+        versionCode = 10
+        versionName = "0.10"
     }
 }
