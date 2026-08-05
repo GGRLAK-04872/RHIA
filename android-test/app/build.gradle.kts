2776d491c05e21761ec5ec8ed46128d3b40517b3
GGRLAK-04872 <brunsmike92@gmail.com>
2026-08-05 18:36:46 +0200

    RHIA Local Test 0.13: start listening after greeting
    * Simplify Android speech startup and remove microphone control
    
    * Bump Android app version to 0.13

--- android-test/app/build.gradle.kts
+++ android-test/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "app.rhia.localtest"
         minSdk = 31
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.12"
+        versionCode = 13
+        versionName = "0.13"
     }
 }
