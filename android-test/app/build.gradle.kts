63032b7c568757a9da2c57a1250741afb4cf64e9
GGRLAK-04872 <brunsmike92@gmail.com>
2026-08-06 09:41:56 +0200

    RHIA 0.24: make local commands resilient to wake-word errors

--- android-test/app/build.gradle.kts
+++ android-test/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "app.rhia.localtest"
         minSdk = 31
         targetSdk = 35
-        versionCode = 23
-        versionName = "0.23"
+        versionCode = 24
+        versionName = "0.24"
     }
 }
 
