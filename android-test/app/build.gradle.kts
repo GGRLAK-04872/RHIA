5d72d2cefe090b4c82325d56a190982660c5de52
GGRLAK-04872 <brunsmike92@gmail.com>
2026-08-06 10:02:06 +0200

    Bump Android test version to 0.25

--- android-test/app/build.gradle.kts
+++ android-test/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "app.rhia.localtest"
         minSdk = 31
         targetSdk = 35
-        versionCode = 24
-        versionName = "0.24"
+        versionCode = 25
+        versionName = "0.25"
     }
 }
 
