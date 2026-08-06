6361a1e93d38171ed0d9bb60c50e4a09a7c6024d
GGRLAK-04872 <brunsmike92@gmail.com>
2026-08-06 08:28:14 +0200

    Release Android test version 0.22

--- android-test/app/build.gradle.kts
+++ android-test/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "app.rhia.localtest"
         minSdk = 31
         targetSdk = 35
-        versionCode = 21
-        versionName = "0.21"
+        versionCode = 22
+        versionName = "0.22"
     }
 }
 
